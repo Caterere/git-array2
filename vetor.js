713c6f79d81bb730501e171514ef3a3ffd11cbfe
Luiz Oro <dadoluizcat@gmail.com>
2023-03-15 09:39:45 -0300

    Incluindo innerHTML para imprimir na tela

--- vetor.js
+++ vetor.js
@@ -1,13 +1,16 @@
 let valor_cidade = [] // variavel global do tipo array
-
+let res = document.getElementById('res')
 
 
 function inserir(){
-    let cidade = document.getElementById('cidade')
+    let cidade = document.getElementById('cidade').value
  valor_cidade.push(cidade)
  console.log(valor_cidade)
+ res.innerHTML += valor_cidade
 }
-function excluir{
-    let cidade = document.getElementById('cidade')
+function excluir(){
     valor_cidade.pop(cidade)
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade
 }
+
